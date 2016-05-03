d85985cdc8606c853688d78ec8909ea4ad3e6a4e
Ron Royston <ron@rack.pub>
2016-05-03 18:30:46 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+<img src="http://www.rack.pub/clockjs/logo.png" alt="Logo" />
+# clock.js
+## Synopsis
+
+Simple datetime tools.  2.39KB minified.  One file.  
+> NOTE: although negligible in most cases, months and years are approximated for the `clock.since` and `clock.until` methods and the `clock.unit.months` and `clock.unit.years` properties.
+
+## Code Example
+```
+clock.now	-->	1462248501241
+clock.time	-->	11:08 PM
+clock.weekday	-->	monday
+clock.day	-->	2
+clock.month	-->	may
+clock.year	-->	2016
+clock.since(1462245888784)	-->	44 minutes
+clock.until(1462255888784)	-->	2 hours
+clock.what.time(1462245888784)	-->	10:24 PM
+clock.what.weekday(1461968554458)	-->	friday
+clock.what.day('14622458887 84')	-->	2
+clock.what.month(1461968554458)	-->	april
+clock.what.year('1461968554458')	-->	2016
+clock.what.time()	-->	11:11 PM
+clock.what.weekday('14619685abcd')	-->	clock.js error : expected unix timestamp as argument
+clock.unit.seconds	-->	1000
+clock.unit.minutes	-->	60000
+clock.unit.hours	-->	3600000
+clock.unit.days	-->	86400000
+clock.unit.weeks	-->	604800000
+clock.unit.months	-->	2628002880
+clock.unit.years	-->	31536000000
+```
+## Motivation
+
+A need for an easy to use simple and untuitive datetime tools library in a single JavaScript file.
+
+## Installation
+
+Copy and paste.
+
+## API Reference
+
+All methods and properties are shown in the code example.
+> Note the properties are live (via getters), i.e., clock.now returns a current timestamp each time you call it.
+
+## Contribute
+
+If you found a bug, have any questions or want to contribute please let me know, [ron@rack.pub](mailto:ron@rack.pub).
+
+## License
+
+Ron Royston, [www.rack.pub](www.rack.pub), [MIT License](https://en.wikipedia.org/wiki/MIT_License)
---
+++ clock.js
@@ -0,0 +1,291 @@
+/*
+clock.js v0.1 - Simple datetime add-on tools for JavaScript
+Created by Ron Royston, www.rack.pub
+https://github.com/rhroyston/clock-js
+License: MIT
+
+clock.now
+clock.time
+clock.weekday
+clock.day
+clock.month
+clock.year
+clock.since(timestamp)
+clock.until(timestamp)
+clock.what.time(timestamp)
+clock.what.weekday(timestamp)
+clock.what.day(timestamp)
+clock.what.month(timestamp)
+clock.what.year(timestamp)
+clock.unit.seconds
+clock.unit.minutes
+clock.unit.hours
+clock.unit.days
+clock.unit.weeks
+clock.unit.months
+clock.unit.years
+*/
+
+//Revealing Module Pattern (Public & Private) w Public Namespace 'clock'
+var clock = (function() {
+    var weekdays = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+    var months = ['january','february','march','april','may','june','july','august','september','october','november','december'];
+    
+    // object to expose as public properties and methods such as clock.now
+    var pub = {};
+    // instantiates public methods such as clock.what.day(timestamp)
+    pub.what = new What;
+    pub.unit = new Unit;
+
+    //clock.now
+    Object.defineProperty(pub, "now", {
+        get: function () { 
+            return Date.now();
+        }
+    });
+
+    //clock.time
+    Object.defineProperty(pub, "time", {
+        get: function () {
+            var d = new Date();
+            var h = d.getHours();
+            var m = ('0' + d.getMinutes()).slice(-2);
+            if(h<12){
+                return h + ':' + m + ' AM';
+            } else {
+                return (h - 12) + ':' + m + ' PM';
+            }
+        }
+    });
+
+    //clock.weekday
+    Object.defineProperty(pub, "weekday", {
+        get: function () {
+            
+            var d = new Date();
+            for (var i = 0; i < weekdays.length; i++) {
+                if( d.getDay() === i ){
+                    return (weekdays[i]);
+                }
+            }
+        }
+    });
+    
+    //clock.day
+    Object.defineProperty(pub, "day", {
+        get: function () {
+            return new Date().getDate();
+        }
+    });
+    
+    //clock.month
+    Object.defineProperty(pub, "month", {
+        get: function () {
+            var d = new Date();
+            for (var i = 0; i < months.length; i++) {
+                if( d.getMonth() === i ){
+                    return (months[i]);
+                }
+            }
+        }
+    });
+
+    //clock.year
+    Object.defineProperty(pub, "year", {
+        get: function () {
+            return new Date().getUTCFullYear();
+        }
+    });
+    
+    //clock.since
+    pub.since = function (timestamp) {
+        var ts = validate(timestamp);
+        if(ts){
+            var seconds = Math.floor((new Date() - ts) / 1000);
+            return timeframe(seconds);
+        }
+    };
+    
+    //clock.until
+    pub.until = function (timestamp) {
+        var ts = validate(timestamp);
+        if(ts){
+            var seconds = Math.floor((ts - new Date()) / 1000);
+            return timeframe(seconds);
+        }
+    };
+
+    //Constructor for methods such as clock.what.day(timestamp)
+    function What () {
+        //clock.what.time(timestamp)
+        this.time = function(timestamp) {
+            var ts = validate(timestamp);
+            if(ts){
+                var d = new Date(ts);
+                var h = d.getHours();
+                var m = ('0' + d.getMinutes()).slice(-2);
+                if(h<12){
+                    return h + ':' + m + ' AM';
+                } else {
+                    return (h - 12) + ':' + m + ' PM';
+                }
+            }
+        };
+        
+        //clock.what.weekday(timestamp)
+        this.weekday = function(timestamp) {
+            var ts = validate(timestamp);
+            if(ts){
+                var d = new Date(ts);
+                for (var i = 0; i < weekdays.length; i++) {
+                    if( d.getDay() === i ){
+                        return (weekdays[i]);
+                    }
+                }                   
+            }
+        };
+        
+        //clock.what.day(timestamp)
+        this.day = function(timestamp) {
+            var ts = validate(timestamp);
+            if(ts){
+                return new Date(ts).getDate();
+            }
+        };
+
+        //clock.what.month(timestamp)
+        this.month = function(timestamp) {
+            var ts = validate(timestamp);
+            if(ts){
+                var d = new Date(ts);
+                for (var i = 0; i < months.length; i++) {
+                    if( d.getMonth() === i ){
+                        return (months[i]);
+                    }
+                }                  
+            }
+        };
+        
+        //clock.what.year(timestamp)
+        this.year = function(timestamp) {
+            var ts = validate(timestamp);
+            if(ts){
+                return new Date(ts).getUTCFullYear();
+            }
+        };
+        
+    } // end of Units Constructor
+    
+    //Constructor for methods such as clock.unit.day
+    function Unit () {
+        this.years = 1000 * 60 * 60 * 24 * 365;
+        this.months = 1000 *60 *60 * 24 * 30.4167;
+        this.weeks = 1000 * 60 * 60 * 24 * 7;
+        this.days = 1000 * 60 * 60 * 24;
+        this.hours = 1000 * 60 * 60;
+        this.minutes = 1000 * 60;
+        this.seconds = 1000;
+    } // end of Units Constructor
+    
+    // private method
+    function timeframe (seconds) {
+        var interval = Math.floor(seconds / 31536000);
+        if (interval > 1) {
+            return interval + " years";
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + " months";
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes";
+        }
+        return Math.floor(seconds) + " seconds";
+    }
+    
+    // private method
+    function isValid(timestamp){
+        var c = 'clock.js error';
+        var expectedTimestamp = {name:c, message:'expected unix timestamp as argument'};
+        var noStrings = {name:c, message:'string arguments not allowed; remove quotes from timestamp'};
+        var noNulls = {name:c, message:'null argument disallowed; use clock.day to get current day'};
+        var notadate = {name:c, message:'argument not a valid Date'};
+        
+        try {
+            if(timestamp == null || timestamp == '') {
+                //or simply return the current unit
+                throw noNulls;
+            }
+            if (typeof timestamp == 'string') {
+                if (/^\d+$/.test(timestamp)){ //all chars numbers
+                    throw noStrings;
+                } else {
+                    throw expectedTimestamp;
+                }
+            }
+            if (new Date(timestamp).getTime() <= 0) {
+                throw notadate;
+            }
+            if (typeof timestamp !== 'number') {
+                throw 'argument should be unix timestamp';
+            }
+            return true;
+        }
+        catch (e) {
+            console.log(e.name + ' : ' + e.message);
+        }
+    }
+    
+    
+    // private method
+    function validate(timestamp){
+        //Error Object Custom Properties
+        var c = 'clock.js error';
+        var expectedTimestamp = {name:c, message:'expected unix timestamp as argument'};
+        
+        try {
+            if(timestamp == null || timestamp == '') {
+                //return current timestamp
+                return pub.now;
+            }
+            if (typeof timestamp == 'string') {
+                //remove whitespace
+                timestamp = timestamp.replace(/\s/g, '');
+                //if all chars numbers
+                if (/^\d+$/.test(timestamp)){
+                    //convert to number
+                    timestamp = Number(timestamp);
+                } else {
+                    throw expectedTimestamp;
+                }
+            }
+            //if timestamp is valid
+            if(new Date(timestamp).getTime() > 0){
+                return timestamp;
+            } else {
+                throw expectedTimestamp;
+            }
+        }
+        catch (e) {
+            console.log(e.name + ' : ' + e.message);
+            return null;
+        }
+    }    
+    
+    //API
+    return pub;
+}());
+
+
+
+    
+    
